fc06c3ceb80a22d628209b431a3a71d2a83c6b5d
Azad Asanali <azadxperias2012@gmail.com>
2018-03-27 12:54:48 +0530

    Initial commit

---
+++ src/main/java/com/neotechlabs/assignment/jeeassignment/JeeAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.neotechlabs.assignment.jeeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeAssignmentApplication.class, args);
+	}
+}
